a33b31a1d66320f8e765f0afca6b5f105baa0b15
t-phill <taylor-phillips>
2019-04-17 19:53:30 -0500

    commenting

--- src/brooks_scrape.py
+++ src/brooks_scrape.py
@@ -9,17 +9,16 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import random
-
 import time
 import csv
-
 import re
-
 import os
+
+# Python script to scrape 'playercard' information from BrooksBaseball
+
 chromedriver = "/Applications/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 
@@ -28,26 +27,31 @@ driver = webdriver.Chrome(chromedriver)
 driver.get("http://www.brooksbaseball.net/landing.php?player=592094")
 
 
+#Initialize empty dictionary and import desired ids
 playercarddict = {}
 df = pd.read_pickle('2018_ids.pkl')
-# df = df.set_index(np.arange(len(df)))
+
+
     
 for index, row in df.iterrows():
     try:
+        #generate playerids from imported id file
         player = df.iloc[index]['name_first'] + ' ' + df.iloc[index]['name_last']
         playerid = df.iloc[index]['key_mlbam']
         
+        #generate soup website link with playerid
         page = "http://www.brooksbaseball.net/landing.php?player=" + str(playerid)
 
         driver.get(page)
-        
         soup = BeautifulSoup(driver.page_source, 'html.parser')
+        #scrape player description
         row = soup.find(class_='well').find('p').text.strip()
-        
-        
+        #add test to dictionary
         playercarddict[player] = row
     except:
+        #skip nonexistant playercards
         continue
 
+#dump playercarddict to pickle file
 with open('playercard.pkl', 'wb+') as handle:
     pickle.dump(playercarddict, handle, protocol=pickle.HIGHEST_PROTOCOL)
